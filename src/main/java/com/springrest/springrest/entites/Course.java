794ea125aa14345d52b27e351cb718c733705fab
Nian007 <angralnikhil99@gmail.com>
2021-01-17 14:49:09 +0530

    some-features

--- src/main/java/com/springrest/springrest/entites/Course.java
+++ src/main/java/com/springrest/springrest/entites/Course.java
@@ -1,14 +1,22 @@
 package com.springrest.springrest.entites;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Course {
+    @Id
     private Long id;
+
     private String title;
+
     private String description;
 }
